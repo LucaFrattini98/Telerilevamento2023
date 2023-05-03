b1b68c915fd5170de1af126ba9ab71b0e9382a56
LucaFrattini98 <128476494+LucaFrattini98@users.noreply.github.com>
2023-05-03 12:21:54 +0200

    Update and rename R_Code_RGB to R_Code_RGB.R

--- R_Code_RGB.R
+++ R_Code_RGB.R
@@ -1,3 +1,53 @@
+# 24/03 -------------------------------------------------------------------------------------------------------------------------
+
+# Insatlling packeges
+install.packages("raster")
+library(raster)
+
+# setting working directory
+setwd("C:/Lab/")
+
+# importing image with all its bands
+l2011 <- brick("p224r63_2011_masked.grd") # importing image with all its bands
+
+# plotting only the 4th band (NIR)
+plot(l2011[[4]]) 
+
+# visualizing an image with natural colors associating the landsat bands to the RGB ones
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+# associating the landsat NIR band (band number 4) to the Red band of RGB, in this way i can see thr infrared
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# associating the NIR band to the color green
+plotRGB(l2011, r=3, g=4, b=2, stretch="Lin")
+
+# associating the NIR band to the color blue
+plotRGB(l2011, r=3, g=2, b=4, stretch="Lin") # stretch=Lin stretches the values in a linear way, this helps to have a better visualization of the image
+
+l1988 <- brick("p224r63_1988_masked.grd")
+plot(l1988[[4]])
+
+# i can divide the plot image in rows and columns (multiframe)
+par(mfrow=c(2,1)) # mf stands for multiframe, 2 indicates the numbers of rows, 1 the numbers of columns
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+
+# I can write the code also without writing r, g, b but just with the band numbers
+plotRGB(l2011, 4, 3, 2, stretch="Lin")
+
+# to clear the plot wiev
+dev.off()
+
+# image strech differences
+par(mfrow=c(2,2))
+plotRGB(l1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(l1988, r=4, g=3, b=2, stretch="Hist")
+plotRGB(l2011, r=4, g=3, b=2, stretch="Hist")
+
+# -----------------------------------------------------------------------------------
+
 # Questo è il primo script che useremo a lezione
 
 # install.packages("raster")
